52fe3d1e56b6fe2d959819b98dddcf73959e9aa4
chobkyu <98572357+chobkyu@users.noreply.github.com>
2023-11-23 11:01:57 +0900

    update

--- main.go
+++ main.go
@@ -2,17 +2,60 @@ package main
 
 import (
 	"fmt"
-	"learngo/github.com/serranoarevalo/learngo/accounts"
+	"learngo/github.com/serranoarevalo/learngo/myDict"
 )
 
+//account ex
+// func main() {
+// 	account := accounts.NewAccout("chobkyu")
+// 	account.Deposit(10)
+// 	fmt.Println(account.Balance())
+// 	err := account.Withdraw(20)
+// 	if err != nil {
+// 		//log.Fatalln(err) //에러나면 프로그램 종료
+// 		fmt.Println(err)
+// 	}
+// 	fmt.Println(account.String())
+// }
+
 func main() {
-	account := accounts.NewAccout("chobkyu")
-	account.Deposit(10)
-	fmt.Println(account.Balance())
-	err := account.Withdraw(20)
+	// dictionary := myDict.Dictionary{"first": "firstword"}
+	// definition, err := dictionary.Search("first")
+
+	// if err != nil {
+	// 	fmt.Println(err)
+	// } else {
+	// 	fmt.Println(definition)
+	// }
+
+	dictionary := myDict.Dictionary{}
+	err := dictionary.Add("hi", "hello")
+
 	if err != nil {
-		//log.Fatalln(err) //에러나면 프로그램 종료
 		fmt.Println(err)
 	}
-	fmt.Println(account.String())
+	definition, err := dictionary.Search("hi")
+
+	fmt.Println(definition)
+
+	err2 := dictionary.Update("dsfsadf", "bye")
+	if err2 != nil {
+		fmt.Println(err2)
+	}
+
+	word, _ := dictionary.Search("hi")
+	fmt.Println(word)
+
+	testWord, _ := dictionary.Search("hi")
+	dictionary.Delete("hi")
+
+	check, err := dictionary.Search("hi")
+	fmt.Println(testWord)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(check)
+
 }
