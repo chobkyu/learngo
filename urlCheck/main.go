274264bfff3e4d3ecaa34ae1e64eab29d6e13633
chobkyu <98572357+chobkyu@users.noreply.github.com>
2023-11-23 14:28:46 +0900

    add goroutines

--- urlCheck/main.go
+++ urlCheck/main.go
@@ -9,6 +9,11 @@ import (
 
 var errRequestFailed = errors.New("Request failed")
 
+type requestResult struct {
+	url    string
+	status string
+}
+
 func main() {
 	urls := []string{
 		"https://www.airbnb.com/",
@@ -21,44 +26,46 @@ func main() {
 		"https://academy.nomadcoders.co/",
 	}
 
-	//result := map[string]string{} //맵은 이렇게 만들거나
-	var results = make(map[string]string) //이런식으로 초기화를 해줘야 값을 넣을 수 있음
+	//results := map[string]string{} //맵은 이렇게 만들거나
+	results := make(map[string]string) //이런식으로 초기화를 해줘야 값을 넣을 수 있음
+	c := make(chan requestResult)
 	for _, url := range urls {
-		result := "OK"
-		err := hitURL(url)
-		if err != nil {
-			result = "FAILED"
-		}
-		results[url] = result
+		go hitURL(url, c)
 	}
 
-	for url, result := range results {
-		fmt.Println(url, result)
-
+	for i := 0; i < len(urls); i++ {
+		//fmt.Println(<-c)
+		result := <-c
+		results[result.url] = result.status
 	}
 
-	c := make(chan string)
-	people := [4]string{"chobkyu", "bk", "cho", "minsu"}
-	for _, person := range people {
-		go isSexy(person, c)
+	for url, status := range results {
+		fmt.Println(url, status)
 	}
 
-	for i := 0; i < len(people); i++ {
-		fmt.Println(<-c)
-	}
+	// c := make(chan string)
+	// people := [4]string{"chobkyu", "bk", "cho", "minsu"}
+	// for _, person := range people {
+	// 	go isSexy(person, c)
+	// }
+
+	// for i := 0; i < len(people); i++ {
+	// 	fmt.Println(<-c)
+	// }
 
 	//time.Sleep(time.Second * 5)
 }
 
-func hitURL(url string) error {
+func hitURL(url string, c chan<- requestResult) { //<- 표시로 채널에서 데이터를 받을 수는 없고 보낼 수만 있다고 지정하는 것
 	fmt.Println("checking : ", url)
 	resp, err := http.Get(url)
+	status := "OK"
 	if err != nil || resp.StatusCode >= 400 {
 		fmt.Println(err, resp.StatusCode)
-		return errRequestFailed
+		status = "FAILED"
 	}
+	c <- requestResult{url: url, status: status}
 
-	return nil
 }
 
 func isSexy(person string, c chan string) {
